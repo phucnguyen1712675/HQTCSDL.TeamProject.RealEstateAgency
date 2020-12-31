fe0e460c0483e0fb66f7ffa366b31301fe308ddd
Sammatha99 <38234300+Sammatha99@users.noreply.github.com>
2020-12-31 16:37:53 +0700

    + note Function To run: chi nhánh, chủ nhà

---
+++ HQTCSDL.TeamProject.RealEstateAgency/View/ChiNhanhScreens/Agency_IsHousePriceIncreases.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HQTCSDL.TeamProject.RealEstateAgency.View.ChiNhanhScreens
+{
+    /// <summary>
+    /// Interaction logic for Agency_IsHousePriceIncreases.xaml
+    /// </summary>
+    public partial class Agency_IsHousePriceIncreases : Window
+    {
+        public Agency_IsHousePriceIncreases()
+        {
+            InitializeComponent();
+        }
+    }
+}
